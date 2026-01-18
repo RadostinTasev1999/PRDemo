6ff3d9e787ba146ab9580070306a351b365984b5
Radostin Tasev <radostintasev22@yahoo.com>
2026-01-18 09:29:44 +0200

    Implement Calculator class with basic operations

---
+++ calculator.js
@@ -0,0 +1,37 @@
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= value;
+    return this;
+  }
+
+  clear() {
+    this.result = 0;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
+}
